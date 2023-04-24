8d9dcf578e8e30dbd95942181bb40a1396ebad9b
carlosg <carlosenrique.gomezgomez@telefonica.com>
2023-04-24 10:45:00 +0200

    admin password fixed

--- src/rpm/SOURCES/opt/ss/develenv/platform/sonar/bin/users/change_admin_password.sh
+++ src/rpm/SOURCES/opt/ss/develenv/platform/sonar/bin/users/change_admin_password.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
+new_password=${1:?}
 netrc_file=/opt/ss/develenv/platform/sonar/conf/.admin_password
 old_password=$(grep -Po "(?<= password ).*" "${netrc_file:?}")
 curl -fs curl --netrc-file "${netrc_file:?}" -X POST \
-"http://localhost/api/users/change_password?login=admin&password=${password:?}&previousPassword=${old_password:?}"
-echo "machine localhost login admin password ${password:?}" > /opt/ss/develenv/platform/sonar/conf/.admin_password
+"http://localhost/api/users/change_password?login=admin&password=${new_password:?}&previousPassword=${old_password:?}"
+echo "machine localhost login admin password ${new_password:?}" > /opt/ss/develenv/platform/sonar/conf/.admin_password
 echo "Password changed"
